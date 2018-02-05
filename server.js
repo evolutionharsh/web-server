ae58dde6d295980da36ec9107ab80202551d0cb4
Harsh Pathania <akshatrana2007@gmail.com>
2018-02-04 23:41:03 -0700

    Init Repo

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.get('/',function(req,res){
+    res.send('This is my Rest Api!');
+});
+app.listen(PORT, function(){
+    console.log('Express listening on port ' + PORT + '!');
+});
